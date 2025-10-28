a9ca8af1dd2939fa7f76be0095fc10fcd6cbf908
Juff-Ma <contact@juffma.de>
2025-10-28 18:09:38 +0100

    Update license headers [skip ci]

--- MsiEngine.cs
+++ MsiEngine.cs
@@ -1,16 +1,15 @@
 ﻿#region copyright
 /*
-    MsiEngine.cs is part of SimpleMSI.
-    Copyright (C) 2025 Julian Rossbach
+MsiEngine.cs is part of SimpleMSI.
+Copyright (C) 2025 Julian Rossbach
 
-    This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 #endregion
-
 using WixSharp;
 using WixSharp.CommonTasks;
 using WixToolset.Mba.Core;
